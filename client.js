3c42ca62ee0b6a989d2deb429d51f0f7ca89eef6
Sam Bahta <smabahta37@gmail.com>
2018-07-28 15:48:35 -0500

    Table Working/semistyled

--- client.js
+++ client.js
@@ -1,6 +1,29 @@
 console.log('JS');
-$(readyNow)
+$(readyNow);
 function readyNow() {
     console.log('JQ');
-    
+    let tBody = $('<tbody id="calcBody"></tbody>');
+    $('#empTable').append(tBody);
+
+
+class employee {
+    constructor(firstNameIn, lastNameIn, idIn, titleIn, annualSalaryIn) {
+        this.firstName = firstNameIn;
+        this.lastName = lastNameIn;
+        this.id = idIn;
+        this.title = titleIn;
+        this.annualSalary = annualSalaryIn;
+    }
+}
+
+const empOne = new employee('Sam', 'Bahta', 001, 'CEO', 150000);
+const empTwo = new employee('Amalia', 'Ellison', 002, 'Legal Counsel', 60000);
+const employeeArr = [empOne, empTwo];
+
+
+
+for (employ of employeeArr) {
+    $('#calcBody').append('<tr>' + '<td>' + employ.firstName + '</td>' + '<td>' + employ.lastName + '</td>' + '<td>' + employ.id + '</td>' + '<td>' + employ.title + '</td>' + '<td>' + employ.annualSalary + '</td>' + '</tr>');
+    console.log(employ);
+}
 }
